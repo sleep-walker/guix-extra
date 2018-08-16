4eddf5c9b15f16cc7335745151c8ae4a30457a81
Tomáš Čech <sleep_walker@gnu.org>
2018-08-16 11:30:00 +0200

    add my-p7zip, my-curl, my-icecat, my-conkeror
    * my-p7zip - do not strip RAR support
    * my-curl - use MIT kerberos support
    * my-icecat - less restrictive stripping of features
    * my-conkeror - using my-icecat instead

--- gnu/packages/my-own.scm
+++ gnu/packages/my-own.scm
@@ -20,21 +20,30 @@
   #:use-module (gnu packages)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages conkeror)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages gnuzilla)
+  #:use-module (gnu packages kerberos)
   #:use-module (gnu packages suckless)
   #:use-module (gnu packages gettext)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages readline)
+  #:use-module (gnu packages tls)
   #:use-module (guix build-system cmake)
   #:use-module (guix download)
   #:use-module (guix packages)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix utils)
+  #:use-module (srfi srfi-1)
   )
 
 (define-public my-conkeror
   (package
    (inherit conkeror)
    (name "my-conkeror")
+    ;; (inputs
+    ;;  `(("my-icecat" ,my-icecat)
+    ;;    ,@(alist-delete "icecat" (package-inputs conkeror))))
    (source
     (origin
      (inherit (package-source conkeror))
@@ -44,7 +53,6 @@
   (package
    (inherit dwm)
    (name "my-dwm")
-   (version "6.0sw")
    (source
     (origin
      (inherit (package-source dwm))
@@ -52,17 +60,68 @@
 		    (search-patch "dwm-r1615-selfrestart.diff")
 		    (search-patch "dwm-6.0-sw.patch")))))))
 
+(define-public my-curl
+  (package
+    (inherit curl)
+    (name "my-curl")
+    (inputs
+     `(("mit-krb5" ,mit-krb5)
+       ,@(alist-delete "gss" (package-inputs curl))))
+    (arguments
+     (substitute-keyword-arguments (package-arguments curl)
+       ((#:configure-flags flags)
+        `(map (lambda (flag)
+                (if (string=? flag "--with-gssapi")
+                    (string-append "--with-gssapi=" (assoc-ref %build-inputs "mit-krb5"))
+                      flag))
+                ,flags))))
+    ))
+
+(define-public my-icecat
+  (package
+    (inherit icecat)
+    (name "my-icecat")
+    (arguments
+     (substitute-keyword-arguments (package-arguments icecat)
+                                   ((#:configure-flags flags)
+       ;; `((use-modules (srfi srfi-1))
+       ;;   (filter (lambda (flag)
+       ;;            (not (any (lambda (str)
+       ;;                        (string=? flag str))
+       ;;                      '("--disable-eme"
+       ;;                        "--with-distribution-id=org.gnu"
+       ;;                        "--disable-tests"
+       ;;                        "--disable-updater"
+       ;;                        "--disable-crashreporter"
+       ;;                        "--disable-maintenance-service"))))
+       ;;           ,flags:)))))
+        `(map (lambda (flag)
+                (if (string=? flag "--disable-eme")
+                    "--enable-eme=widevine"
+                    flag))
+                ,flags))))
+    ))
+
+
+(define-public my-p7zip
+  (package
+   (inherit p7zip)
+   (name "my-p7zip")
+   (source
+    (origin
+      (inherit (package-source p7zip))
+      (snippet #f)))))
+
 (define-public sdcv
   (package
    (name "sdcv")
-   (version "0.5.0-beta4")
+   (version "0.5.2")
    (source
     (origin
      (method url-fetch)
-     (uri (string-append "mirror://sourceforge/project/sdcv/sdcv/sdcv-"
-			 version "-Source.tar.bz2"))
+     (uri (string-append "https://github.com/Dushistov/" name "/archive/v" version ".tar.gz"))
      (sha256
-      (base32 "1b9v91al2c1499q6yx6q8jggid0714444mfj6myqgz3nvqjyrrqr"))))
+      (base32 "1xy48ghd458g00cd8p19cq1rpw4lyaxj06phknldaj07984dq27c"))))
    (build-system cmake-build-system)
    (native-inputs
     `(("pkg-config" ,pkg-config)))
