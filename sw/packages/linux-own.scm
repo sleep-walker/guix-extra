884e65dcae5df034d1f2b9c158b8fe7634a137ea
Tomáš Čech <sleep_walker@gnu.org>
2023-09-12 23:41:48 +0200

    use sw-corrupt

--- sw/packages/linux-own.scm
+++ sw/packages/linux-own.scm
@@ -76,7 +76,7 @@ on hardware which requires nonfree software to function."))))
 
 
 (define-public linux-doom
-  (corrupt-linux linux-libre-6.4
+  (sw-corrupt-linux linux-libre-6.4
 		 #:name "linux-doom"
 ;;		 #:configuration-file "/Devel/git/guix-extra/sw/packages/aux-files/kernel-doom-sw8.config"
 		 ))
